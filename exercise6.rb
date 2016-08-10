c28be539a191b66ddf040b681c212e1d719d7932
Wendelcorp <bryce@brycewendelaar.com>
2016-08-10 16:01:47 -0400

    Made improvements to exercise 6

--- exercise6.rb
+++ exercise6.rb
@@ -1,5 +1,16 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
-grocery_list.each do |star|
-  puts "* #{star}"
+grocery_list << "rice"
+
+grocery_list.length
+
+grocery_list.delete(3)
+
+
+def output_once(list)
+  list.sort.each do |star|
+    puts "* #{star}"
+  end
 end
+
+output_once(grocery_list)
