026f9dcc954381f53cf38c9109045569d1196c9a
Wendelcorp <bryce@brycewendelaar.com>
2016-08-10 15:29:10 -0400

    Made improvements to exercise 7

--- exercise7.rb
+++ exercise7.rb
@@ -4,6 +4,22 @@ students = {
   :cohort3 => 22
 }
 
+students[:cohort4] = 43
+
+students.values do |k|
+  k * 1.05
+end
+
+students.delete(:cohort2)
+
 students.each do |key, value|
   puts "#{key}: #{value} students"
 end
+
+sum = 0
+
+students.each do |key, value|
+  sum += value
+end
+
+puts sum
